bd5866a1c9c49c1f61743958ba4d63de8aab9358
Helena <helena@klarr.se>
2025-01-09 12:01:11 +0100

    finished post and get for authors

---
+++ src/main/java/com/java24/bookshop/controllers/AuthorController.java
@@ -0,0 +1,75 @@
+package com.java24.bookshop.controllers;
+
+import com.java24.bookshop.models.Author;
+import com.java24.bookshop.repositories.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/authors")
+public class AuthorController {
+    @Autowired
+    private AuthorRepository authorRepository;
+
+    // HTTP POST METOD
+    @PostMapping
+    public ResponseEntity<Author> createAuthor(@RequestBody Author author) {
+        Author savedAuthor = authorRepository.save(author);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedAuthor);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Author>> getAllAuthors() {
+        List<Author> authors = authorRepository.findAll();
+        return ResponseEntity.ok(authors);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Author> getAuthorById(@PathVariable String id) {
+        Author author = authorRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Author not found"));
+        return ResponseEntity.ok(author);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
